5547dea28331cc090b8e1e8cf16550bb0deb64fa
trob0697 <tahreak.robinson@gmail.com>
2024-05-03 21:57:23 -0400

    shadcn theme and button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span>Hello World</span>
+      <Button>Click me</Button>
     </main>
   );
 }
