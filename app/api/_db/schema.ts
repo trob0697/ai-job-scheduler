9c637c4f558d3bb8863539a8e128714c4746d9e6
trob0697 <tahreak.robinson@gmail.com>
2024-05-06 22:09:41 -0400

    update db

--- app/api/_db/schema.ts
+++ app/api/_db/schema.ts
@@ -1,8 +1,9 @@
-import { pgTableCreator, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgTableCreator, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const createTable = pgTableCreator((name) => `${name}`);
 
-export const jobs = createTable("jobs", {
+export const jobs = createTable("stuff", {
   id: uuid("id").defaultRandom().primaryKey(),
   createdAt: timestamp("createdAt").defaultNow(),
+  name: text("name").notNull(),
 });
