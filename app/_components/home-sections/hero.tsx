44946b4c6aab15a02ca974ffe8dbb63ea559b97d
trob0697 <tahreak.robinson@gmail.com>
2024-05-08 18:28:09 -0400

    hero page created

---
+++ app/_components/home-sections/hero.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default async function Hero() {
+  return (
+    <div className="nav-section">
+      <div className="flex-col space-y-4">
+        <h1>Bold & Powerful Heading</h1>
+        <div>
+          One to two sentences explaining the product to the target audience
+        </div>
+        <Button>Button</Button>
+      </div>
+      <Image
+        src={"/random.jpg"}
+        width={600}
+        height={300}
+        alt="random picture"
+      />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
-import { getAllStuff } from "./api/_db/stuff";
+import Hero from "./_components/home-sections/hero";
 
 export default async function Home() {
-  const stuff = await getAllStuff();
-
   return (
-    <div className="flex flex-col items-center p-24">
-      <span>Hello World</span>
-      <div className="space-x-2">
-        {stuff.map((item) => {
-          return <span>{item.name}</span>;
-        })}
-      </div>
+    <div className="flex flex-col items-center p-36">
+      <Hero />
     </div>
   );
 }
